73a382b9e3a68ffdebfe2b916e2f159d00fd50fa
ljwrer <ljwrer@sina.com>
2016-09-18 18:07:06 +0800

    'chart'

--- file-explorer/fs.js
+++ file-explorer/fs.js
@@ -2,14 +2,17 @@
  * Created by Ray on 2016/9/14.
  */
 const fs=require('fs');
-// fs.readFile('./deep.js','utf8',function (err,content) {
-//     console.log(content)
-// });
 let stream=fs.createReadStream('./d3.js','utf-8');
 let i=0;
 stream.on('data',function (chunk) {
-    console.log(i++,':',chunk.slice(0,10));
+    console.log(i+++":",chunk.slice(0,30));
 });
 stream.on('end',function (data) {
-    console.log(data);
+    console.log('end');
+});
+const files=fs.readdirSync(process.cwd());
+files.forEach(function (filename) {
+    fs.watchFile(process.cwd()+'/'+filename,function () {
+        console.log('-',filename,'changed');
+    })
 })
--- tcp-chart/index.js
+++ tcp-chart/index.js
@@ -1,10 +1,49 @@
 const colors = require('colors/safe');
-const theme=require('colors/themes/generic-logging');
+const theme = require('colors/themes/generic-logging');
 colors.setTheme(theme);
-const net=require('net');
-var server=net.createServer(function (conn) {
-   console.log(colors.silly('server listen on *:3000'))
+const net = require('net');
+let count = 0;
+let users={};
+var server = net.createServer(function (conn) {
+    conn.setEncoding('utf8');
+    console.log(colors.silly('new connection!'));
+    conn.write(colors.silly('welcome to node-chart \nthere are '+count+' people\nplease write your name and press enter:'))
+    count++;
+    let nickName;
+    const broadcast=function (msg,exceptMe) {
+        for (let i in users) {
+            if (!exceptMe || i!==nickName){
+                users[i].write(colors.silly(msg)+'\n')
+            }
+        }
+    };
+    conn.on('data',function (data) {
+        data=data.replace(/[\r\n]/g,'');
+        if(!nickName){
+            if(users[data]){
+                conn.write(data+' already in use,try again');
+                return;
+            }else {
+                nickName=data;
+                users[nickName]=conn;
+                broadcast(nickName+" join in");
+            }
+        }else {
+            broadcast(nickName+':'+data);
+        }
+        console.log(nickName+':'+data);
+    });
+    conn.on('close', function () {
+        count--;
+        if(nickName){
+            delete users[nickName];
+            broadcast(nickName + ' left the room');
+        }
+    });
+    conn.on('error',function () {
+
+    })
 });
-server.listen(3000,function (conn) {
+server.listen(3000, function (conn) {
     console.log('server listening on *:3000')
 });
