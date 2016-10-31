9d59f6f0ebfd00803e873c97e483ad3183701d4e
ljwrer <ljwrer@sina.com>
2016-10-31 18:08:17 +0800

    sq

--- connect/connect.js
+++ connect/connect.js
@@ -5,14 +5,15 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 const compression = require('compression');
 const morgan = require('morgan');
+const qs=require('qs');
 const path = require('path');
 const time = require('./request-time');
 const app = connect();
 app.use(compression());
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
 app.use(time({time:500}));
 app.use(serveStatic(path.join(__dirname, 'static')));
-app.use(function (req,res,next) {
+app.use(function a_d(req,res,next) {
     if('/a'===req.url){
         res.writeHead(200);
         res.end('quick');
@@ -20,7 +21,7 @@ app.use(function (req,res,next) {
         next()
     }
 });
-app.use(function (req,res,next) {
+app.use(function b_d(req,res,next) {
     if('/b'===req.url){
         res.writeHead(200);
         setTimeout(function () {
